bab313c6241ed4fe69c48a05221a7b61c320cced
Kaushik Bhut <ravimaradiya0@gmail.com>
2026-02-12 19:13:34 +0530

    my order js change and Wishlist add on wishlist section

--- static/js/index.js
+++ static/js/index.js
@@ -116,6 +116,10 @@ function showinformation() {
     my_order.classList.remove("active");
 
 }
+function showrenderOrders() {
+    my_order.classList.add("active");
+    renderOrders();
+}
 
 window.addEventListener("scroll", () => {
     if (window.scrollY > 10) {
@@ -666,7 +670,7 @@ async function renderDetails() {
 
                     <div class="step current">
                         <div class="circle">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"       stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-truck" aria-hidden="true"><path d="M14 18V6a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v11a1 1 0 0 0 1 1h2"></path><path d="M15 18H9"></path><path d="M19 18h2a1 1 0 0 0 1-1v-3.65a1 1 0 0 0-.22-.624l-3.48-4.35A1 1 0 0 0 17.52 8H14"></path><circle cx="17" cy="18" r="2"></circle><circlecx="7" cy="18" r="2"></circle></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"       stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-truck" aria-hidden="true"><path d="M14 18V6a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v11a1 1 0 0 0 1 1h2"></path><path d="M15 18H9"></path><path d="M19 18h2a1 1 0 0 0 1-1v-3.65a1 1 0 0 0-.22-.624l-3.48-4.35A1 1 0 0 0 17.52 8H14"></path><circle cx="17" cy="18" r="2"></circle><circle cx="7" cy="18" r="2"></circle></svg>
                         </div>
                         <h4>Shipped</h4>
                         <span>2026-02-14T06:05</span>
@@ -925,8 +929,8 @@ function loadWishlist() {
 
     wishlist.forEach(product => {
         container.innerHTML += `
-            <div>
-                <img src="${product.image}" width="80">
+            <div class="wishlist_prodect">
+                <img src="${product.image}">
                 <p>${product.price}</p>
                 <h3>₹${product.name}</h3>
                 <button onclick="removeFromWishlist('${product.name}')">
